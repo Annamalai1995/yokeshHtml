540a124a25ddaae525e523eed4e03de01bd67343
Annamalai1995 <s.annamalai1995@gmail.com>
2023-12-01 11:29:55 +0530

    react

---
+++ src/function.js
@@ -0,0 +1,69 @@
+import './External.css'
+function Orderlist()
+{
+    return(
+        <>
+        <ol>
+            name
+            <li>
+                annamalai
+            </li>
+            <li>
+                sathish
+            </li>
+        </ol>
+        </>
+    );
+
+}
+
+function Fun1()
+{
+    return(
+        <>
+        <h1>data flow</h1>
+        </>
+    )
+}
+export let Table1=()=>
+{
+    return(
+        <>
+        <table >
+            <thead>
+                <tr>
+                    <th>S.no</th>
+                    <th>Studentname</th>
+                    <th>Lovers Name</th>
+                    <th>Bestie Names</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Bharath</td>
+                    <td>Sarulatha,Madhumitha,Anitha,Swathy</td>
+                    <td>Manoj,haritha</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Vijeeth</td>
+                    <td>Sarulatha,Madhumitha,Anitha,Swathy</td>
+                    <td>Vicky,Dhenna</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Annamalai</td>
+                    <td>Sarulatha,Madhumitha,Anitha,Swathy</td>
+                    <td>Sathish,Viji</td>
+                </tr>
+            </tbody>
+        </table>
+        
+        </>
+
+    );
+}
+export default Fun1;
+export{Orderlist};
+
